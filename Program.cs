2f57b15ec00b8bd79c1a3484f3e8fbc2715f999c
IvanShmytov <ivanshmytov@mail.ru>
2022-05-14 12:28:49 +0300

    Задание 12.1.5 выполнено.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace FinalTask
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+			User[] arr = new User[] { 
+				new User("12345", "Ivan", false ), 
+				new User("67890", "Sergey", true), 
+				new User("09876", "Victor", false) 
+			};
+            for (int  i = 0;  i < arr.Length;  i++)
+            {
+				arr[i].Greeting();
+                if (arr[i].IsPremium != true)
+                {
+					ShowAds();
+                }
+            }
+        }
+		static void ShowAds()
+		{
+			Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+			Thread.Sleep(1000);
+			Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+			Thread.Sleep(2000);
+			Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+			Thread.Sleep(3000);
+            Console.WriteLine();
+		}
+	}
+}
